6004ef5a851b082e4bf8e744d91a66836b23b984
Larissayk <lari.yukie@gmail.com>
2020-01-05 21:53:48 -0300

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Footer() {
+  return <footer>This is my footer element</footer>;
+}
+
+export default Footer;
---
+++ src/components/MyInfo.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>Larissa</h1>
+      <p>Something about myself.</p>
+      <ol>
+        <li>Alaska</li>
+        <li>New Zealand</li>
+        <li>YellowStone</li>
+      </ol>
+    </div>
+  );
+}
+
+export default MyInfo;
