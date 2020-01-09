50c13c3f7994153d93dfad1145cf1c93ec6d54aa
Larissayk <lari.yukie@gmail.com>
2020-01-08 21:55:30 -0300

    toDoApp phase 6.1

--- src/App.js
+++ src/App.js
@@ -10,10 +10,35 @@ class App extends React.Component {
     this.state = {
       todos: ToDodata
     };
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedState = prevState.todos.map(item => {
+        if (item.id === id) {
+          return {
+            ...item,
+            completed: !item.completed
+          }
+        }
+        return item;
+      });
+      console.log(prevState.todos)
+      console.log("up",updatedState)
+      return {
+        todos: updatedState
+      };
+    });
+  }
+
   render() {
     const ToDoList = this.state.todos.map(item => (
-      <ToDoItem key={item.id} todosData={item} />
+      <ToDoItem
+        key={item.id}
+        todosData={item}
+        handleChange={this.handleChange}
+      />
     )); //todosData é o nome do props que vai ser passado e usado no ToDoItem function. Eu posso dar qualquer nome, mas o mesmo tem que ser usado no props.
 
     return (
--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -2,12 +2,13 @@ import React from "react";
 import "../styles.css";
 
 function ToDoItem(props) {
-  function message(){
-    console.log("Something have changed")
-  }
   return (
     <div className="todo-item">
-      <input type="checkbox" checked={props.todosData.completed} onChange={message} />
+      <input 
+      type="checkbox" 
+      checked={props.todosData.completed} 
+      onChange={()=> props.handleChange(props.todosData.id)} 
+      />
       <label>{props.todosData.text}</label>
       <br />
     </div>
